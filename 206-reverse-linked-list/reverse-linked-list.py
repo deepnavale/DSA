4a38a1fe6dc6699aebf11b5e43dad58cc3147509
Deep Navale <121570926+deepnavale@users.noreply.github.com>
2025-08-07 18:10:33 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (6.62%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -9,11 +9,13 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        prev=None
-        curr=head
-        while curr:
-            tmp=curr.next
-            curr.next=prev
-            prev=curr
-            curr=tmp
-        return prev
+        if not head or not head.next:
+            return head
+
+        newHead= self.reverseList(head.next)
+        head.next.next=head
+        head.next=None
+
+        return newHead
+
+        
