08296a16e94b03fbf7279787363b37129aaa2a20
Deep Navale <121570926+deepnavale@users.noreply.github.com>
2025-10-09 20:49:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.00%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [-1]*46
+        def solve(n):
+            if dp[n]==-1:
+                if n<0 : return 0
+                if n==0 : return 1
+
+                onestep = solve(n-1)
+                twostep = solve(n-2)
+            else:
+                return dp[n]
+            
+            dp[n] = onestep + twostep
+            return dp[n]
+        
+        
+             
+        return solve(n)
