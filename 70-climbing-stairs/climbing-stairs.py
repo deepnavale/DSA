ee152fa4ad93e87edf76da07ed4ef4e1ac81244f
Deep Navale <121570926+deepnavale@users.noreply.github.com>
2025-10-09 21:23:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (54.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -7,10 +7,11 @@ class Solution(object):
         dp = [-1]*46
 
     
-        dp[0]=1
+        dp[0]=0
         dp[1]=1
+        dp[2]=2
 
-        for i in range(2,n+1):
+        for i in range(3,n+1):
             dp[i] = dp[i-1]+dp[i-2]
         return dp[n]
 
