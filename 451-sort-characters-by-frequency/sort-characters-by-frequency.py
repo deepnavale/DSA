46393bf916b033515abb2f8655959fbba903897e
Deep Navale <121570926+deepnavale@users.noreply.github.com>
2025-10-27 17:24:53 +0530

    Time: 14 ms (85.13%) | Memory: 13.6 MB (44.63%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        map = {}
+        for ch in s:
+            map[ch] = map.get(ch,0)+1
+        sorted_map = sorted(map.items(),key=lambda x:x[1],reverse=True)
+        res = []
+        for ch,freq in sorted_map:
+            res.append(ch*freq)
+        return ''.join(res)
+
+    
