b8539712d94c6a68b258181096b667be97e852c8
Deep Navale <121570926+deepnavale@users.noreply.github.com>
2025-11-02 21:17:41 +0530

    Time: 3 ms (29.27%) | Memory: 12.5 MB (9.30%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -4,13 +4,23 @@ class Solution(object):
         :type stones: List[int]
         :rtype: int
         """
-        while(len(stones)>1):
-            stones.sort()
-            x=stones.pop()
-            y=stones.pop()
-            if x!=y:
-                stones.append(x-y)
-        return stones[0] if stones else 0
+        maxheap = [-stone for stone in stones]
+        heapq.heapify(maxheap)
+        while len(maxheap)>1:
+            x=heapq.heappop(maxheap)
+            y= heapq.heappop(maxheap)
+            heapq.heappush(maxheap,(x-y))
+        return -maxheap[0]
+
+
+
+        # while(len(stones)>1):
+        #     stones.sort()
+        #     x=stones.pop()
+        #     y=stones.pop()
+        #     if x!=y:
+        #         stones.append(x-y)
+        # return stones[0] if stones else 0
 
 
 
