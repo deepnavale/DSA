147fd41a95e4ec9d9930aa67ab9ba8e65064f18f
Deep Navale <121570926+deepnavale@users.noreply.github.com>
2026-01-19 22:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (93.47%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = [0]*2*len(nums)
+        n = len(nums)
+        ans[:n]= nums
+        ans[n:]= nums
+        return ans
+        
