af08458b72671b03057a234c737b742d4f78deac
Deep Navale <121570926+deepnavale@users.noreply.github.com>
2026-03-19 13:15:00 +0530

    Time: 9 ms (79.67%) | Memory: 19.3 MB (98.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,68 +2,17 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        # for x in s:
-        #     cntinc = 0
-        #     for y in t:
-        #         if x ==y:   
-        #             t = t.replace(y,'',1)
-        #             cntinc=1
-        #             break
-        #     if cntinc==0:
-        #         return False
-        # return True
-
-        # a = list(s)
-        # b = list(t)
-        # a.sort()
-        # b.sort()
-        # return a==b
-
-        # sfreq = {}
-        # tfreq = {}
-        # for x in s:
-        #     sfreq[x] = sfreq.get(x,0) + 1
-        # for x in t:
-        #     tfreq[x] = tfreq.get(x,0) + 1
+        sdict = {}
+        tdict = {}
+        for char in s:
+            sdict[char] = sdict.get(char,0) + 1
+        for char in t:
+            tdict[char] = tdict.get(char,0) +1
         
-        # for x in list(sfreq):
-        #     if x not in tfreq:
-        #         return False
-        #     else:
-        #         sfreq[x] -= tfreq[x]
-        #         if sfreq[x]==0:
-        #             del sfreq[x]
-        # return not sfreq
-        
-        # for x in sfreq:
-        #     if x not in tfreq:
-        #         return False
-        #     elif sfreq[x]!=tfreq[x]:
-        #         return False
-        # return True
-
-            
-        # for key, val in sfreq.items():
-            # if key in tfreq:
-            #     if sfreq[key]!=tfreq[key]:
-            #         return False
-            # else:
-            #     return False
-
-            # if sfreq[key] != tfreq.get(key,0):
-            #     return False
-        # return True
-
-
-        freq = {}
-        for x in s:
-            freq[x] = freq.get(x,0)+1
-        for x in t:
-            if x not in freq:
+        for k,v in sdict.items():
+            if k not in tdict:
                 return False
-            freq[x] -= 1
-            if freq[x] ==0:
-                del freq[x]
-        return not freq
-            
-                
+            if tdict[k] != v:
+                return False
+        
+        return True
